5fd327d069b633361038fdab7f073e736711fd6b
Алексей <a79347304@gmail.com>
2025-08-30 16:33:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,6 @@ import { Container } from '@/components/container';
 export default function Home() {
   return (
     <>
-      <section className="relative h-[60vh] w-full">
-        <Image
-          src="https://picsum.photos/1920/1080"
-          alt="Delicious food"
-          fill
-          className="object-cover"
-          data-ai-hint="delicious food"
-          priority
-        />
-        <div className="absolute inset-0 bg-black/50" />
-      </section>
-      
       <section id="menu" className="py-16">
         <Container>
           <MenuDisplay />
