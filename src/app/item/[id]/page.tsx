bd5cede066ae725b136d12be24343bcf9ac0ffbb
Алексей <a79347304@gmail.com>
2025-09-05 13:27:22 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -2,22 +2,39 @@
 'use client';
 
 import { menuData } from '@/lib/menu-data';
-import { notFound } from 'next/navigation';
+import { notFound, useParams } from 'next/navigation';
 import { ItemDetails } from '@/components/item-details';
 import type { MenuItemType } from '@/lib/types';
 import { Container } from '@/components/container';
+import React from 'react';
 
-interface ItemPageProps {
-  params: {
-    id: string;
-  };
-}
+export default function ItemPage() {
+  const params = useParams<{ id: string }>();
+  const [item, setItem] = React.useState<MenuItemType | undefined>(undefined);
+  const [loading, setLoading] = React.useState(true);
 
-export default function ItemPage({ params }: ItemPageProps) {
-  const item: MenuItemType | undefined = menuData.find((i) => i.id === params.id);
+  React.useEffect(() => {
+    if (params.id) {
+      const foundItem = menuData.find((i) => i.id === params.id);
+      if (!foundItem) {
+        notFound();
+      } else {
+        setItem(foundItem);
+      }
+    }
+    setLoading(false);
+  }, [params.id]);
+
+  if (loading) {
+    return (
+      <Container className="py-8">
+        <p>Loading...</p>
+      </Container>
+    );
+  }
 
   if (!item) {
-    notFound();
+    return notFound();
   }
 
   return (
