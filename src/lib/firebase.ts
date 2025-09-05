6051494c0a5f6586da89f539e2e18cced967e1e4
Алексей <a79347304@gmail.com>
2025-09-05 20:29:28 +0000

    все firebase config должны быть в env?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: "fast-eats-hub",
-  appId: "1:525071735816:web:f0dc4b4230caa6a7d5c5bd",
-  storageBucket: "fast-eats-hub.firebasestorage.app",
-  apiKey: "AIzaSyAVg9V5LUkPv0ly-VqZY0xAkT5gvM5stp4",
-  authDomain: "fast-eats-hub.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "525071735816"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
